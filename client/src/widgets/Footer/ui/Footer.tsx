21859c9b54610783aa7c6369154b9ced04edabb4
Denis Laluev <laluevdenis@yandex.ru>
2023-09-15 18:22:22 +0300

    Add an adaptive version of the Footer

--- client/src/widgets/Footer/ui/Footer.tsx
+++ client/src/widgets/Footer/ui/Footer.tsx
@@ -7,14 +7,14 @@ const Footer: FC<FooterProps> = () => {
   return (
     <div className="bg-black text-white">
       <div className="pt-12" />
-      <Container className="flex border-t border-white pt-2 text-[13px]">
+      <Container className="flex gap-10 border-t border-white pb-20 pt-2 text-[13px]">
         <p>
           Уникальные проект Санкт-Петербургского государственного университета
           получения практических
           <br /> навыков без отрыва от учебного процесса для решения задач,
           поставленных заказчиком.
         </p>
-        <p className="ml-auto">© СПбГУ, 2023</p>
+        <p className="ml-auto whitespace-nowrap">© СПбГУ, 2023</p>
       </Container>
     </div>
   );
