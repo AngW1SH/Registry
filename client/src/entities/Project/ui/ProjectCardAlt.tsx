3587d231e82a13ec5398830a6aeef9511ce13739
Denis Laluev <laluevdenis@yandex.ru>
2023-09-15 18:14:47 +0300

    Add an adaptive version of the NewProjects widget and fill in static projects

--- client/src/entities/Project/ui/ProjectCardAlt.tsx
+++ client/src/entities/Project/ui/ProjectCardAlt.tsx
@@ -3,13 +3,19 @@ import { FC, ReactNode } from "react";
 interface ProjectCardAltProps {
   className?: string;
   tags?: ReactNode;
+  project: any;
 }
 
-const ProjectCardAlt: FC<ProjectCardAltProps> = ({ className = "", tags }) => {
+const ProjectCardAlt: FC<ProjectCardAltProps> = ({
+  className = "",
+  tags,
+  project,
+}) => {
+  //border-r border-t border-black p-3
   return (
-    <div className={"border-r border-t border-black p-3 " + className}>
+    <div className={"flex flex-col " + className}>
       <div className="flex justify-between">
-        <div className="whitespace-nowrap text-xs font-bold">
+        <div className="w-15 whitespace-nowrap text-xs font-bold">
           <div className="pt-1" />
           <span className="block text-3xl">09</span>
           сен 2023
@@ -19,13 +25,15 @@ const ProjectCardAlt: FC<ProjectCardAltProps> = ({ className = "", tags }) => {
           <h2 className="text-sm font-medium">
             ГМО: история, достижения, социальные и экологические риски
           </h2>
-          {tags && (
-            <>
-              <div className="pt-10" />
-              <div>{tags}</div>
-            </>
-          )}
         </div>
+      </div>
+      <div className="ml-auto sm:ml-0 xl:pl-20">
+        {tags && (
+          <>
+            <div className="pt-5 xl:pt-10" />
+            <div>{tags}</div>
+          </>
+        )}
       </div>
     </div>
   );
--- client/src/widgets/NewProjects/ui/NewProjects.tsx
+++ client/src/widgets/NewProjects/ui/NewProjects.tsx
@@ -2,31 +2,19 @@ import { ProjectCardAlt } from "@/entities/Project";
 import { TagList } from "@/entities/Tag";
 import { LinkWithIcon } from "@/shared/ui";
 import { FC } from "react";
+import { staticProjects } from "../static/staticProjects";
 
 interface NewProjectsProps {}
 
 const NewProjects: FC<NewProjectsProps> = () => {
   return (
     <>
-      <ul className="grid auto-rows-fr grid-cols-3 gap-y-5">
-        <li className="col-span-1 row-span-1">
-          <ProjectCardAlt tags={<TagList />} />
-        </li>
-        <li className="col-span-1 row-span-1">
-          <ProjectCardAlt tags={<TagList />} />
-        </li>
-        <li className="col-span-1 row-span-1">
-          <ProjectCardAlt className="border-r-0" tags={<TagList />} />
-        </li>
-        <li className="col-span-1 row-span-1">
-          <ProjectCardAlt tags={<TagList />} />
-        </li>
-        <li className="col-span-1 row-span-1">
-          <ProjectCardAlt tags={<TagList />} />
-        </li>
-        <li className="col-span-1 row-span-1">
-          <ProjectCardAlt className="border-r-0" tags={<TagList />} />
-        </li>
+      <ul className="grid auto-rows-fr grid-cols-1 gap-y-5 sm:grid-cols-2 lg:grid-cols-3 sm:[&>li:nth-child(2n)]:border-r-0 lg:[&>li:nth-child(2n)]:border-r lg:[&>li:nth-child(3n)]:border-r-0 [&>li]:border-x-0  sm:[&>li]:border-r">
+        {staticProjects.map((project) => (
+          <li className="col-span-1 row-span-1 border-t border-black p-3">
+            <ProjectCardAlt project={project} tags={<TagList />} />
+          </li>
+        ))}
       </ul>
       <div className="pt-6" />
       <LinkWithIcon href="/" className="font-medium" iconSize={10}>
