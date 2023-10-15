743eec37ebb316e2c4246a836725de6bb93f7256
Denis Laluev <laluevdenis@yandex.ru>
2023-10-15 20:58:13 +0300

    Add teamRepository.getUnsassignedAdministratedByUser Repositories are starting to look ugly. Probably going to write some wrapper for strapi requests to avoid duplicating code

--- server/src/repositories/team/__tests__/teamRepository.test.ts
+++ server/src/repositories/team/__tests__/teamRepository.test.ts
@@ -27,4 +27,24 @@ describe("Team Repository", () => {
       );
     });
   });
+
+  describe("getUnassignedAdministratedByUser", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+    it("should return a list of teams when everything is okay", async () => {
+      const user = staticUser;
+
+      const result = await teamRepository.getUnassignedAdministratedByUser(
+        user
+      );
+
+      expect(result).toBeDefined();
+      expect(result).toEqual(
+        expect.objectContaining({
+          data: expect.anything(),
+        })
+      );
+    });
+  });
 });
--- server/src/repositories/team/index.ts
+++ server/src/repositories/team/index.ts
@@ -2,7 +2,10 @@ import { User } from "@/entities/user";
 import qs from "qs";
 
 const teamRepositoryFactory = () => {
-  return Object.freeze({ getUnassignedByUser });
+  return Object.freeze({
+    getUnassignedByUser,
+    getUnassignedAdministratedByUser,
+  });
 
   async function getUnassignedByUser(user: User) {
     const params = {
@@ -16,6 +19,45 @@ const teamRepositoryFactory = () => {
           user: {
             id: user.id,
           },
+        },
+      },
+      populate: {
+        members: {
+          fields: ["id", "name", "role"],
+          populate: {
+            user: {
+              fields: ["id", "name", "email"],
+            },
+          },
+        },
+        administrators: {
+          fields: ["id", "name", "email"],
+        },
+      },
+    };
+
+    const response = await fetch(
+      process.env.STRAPI_URL + "teams?" + qs.stringify(params),
+      {
+        headers: {
+          Authorization: "bearer " + process.env.PROJECTS_TOKEN,
+        },
+      }
+    ).then((data) => data.json());
+
+    return response;
+  }
+
+  async function getUnassignedAdministratedByUser(user: User) {
+    const params = {
+      filters: {
+        project: {
+          id: {
+            $null: true,
+          },
+        },
+        administrators: {
+          id: user.id,
         },
       },
       populate: {
