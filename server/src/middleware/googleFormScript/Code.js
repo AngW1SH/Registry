b611a98c0652bb5ebeb44e8e1decbc4d3653d546
Denis Laluev <laluevdenis@yandex.ru>
2023-10-17 20:33:37 +0300

    Add googleFormScript that sends the form results to the app

---
+++ server/src/middleware/googleFormScript/Code.js
@@ -0,0 +1,40 @@
+// Code.gs
+
+function sendResponse(e) {
+  var data = {
+    form: {
+      id: e.source.getId(),
+      title: e.source.getTitle() ? e.source.getTitle() : "Untitled Form",
+      is_private: e.source.requiresLogin(),
+      is_published: e.source.isAcceptingResponses(),
+    },
+    response: {
+      id: e.response.getId(),
+      email: e.response.getRespondentEmail(),
+      timestamp: e.response.getTimestamp(),
+      data: e.response
+        .getItemResponses()
+        .map(function (y) {
+          return {
+            h: y.getItem().getTitle(),
+            k: y.getResponse(),
+          };
+        }, this)
+        .reduce(function (r, y) {
+          r[y.h] = y.k;
+          return r;
+        }, {}),
+    },
+  };
+
+  var options = {
+    method: "post",
+    payload: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    headers: {
+      "Authorization": "Bearer " + process.env.SUBMIT_FORM_TOKEN
+    }
+  };
+
+  UrlFetchApp.fetch(process.env.WEBSITE_URL + "/api/user/form", options);
+}
