bac833c898ed5443eb1586b8b5902a816e851d1a
Denis Laluev <laluevdenis@yandex.ru>
2023-10-26 15:18:42 +0300

    Redesign UserNav UI

--- client/src/features/UserNav/ui/UserNav.tsx
+++ client/src/features/UserNav/ui/UserNav.tsx
@@ -5,6 +5,7 @@ import { FC, useContext } from "react";
 import { logout } from "../api/logout";
 import { useRouter } from "next/navigation";
 import { useQueryClient } from "@tanstack/react-query";
+import Link from "next/link";
 
 interface UserNavProps {
   text?: "bright" | "dark";
@@ -35,18 +36,49 @@ const UserNav: FC<UserNavProps> = ({ text = "bright" }) => {
 
   return (
     <div className="group relative z-40 cursor-pointer">
-      <div onClick={handleLogin}>
-        <Image
-          src={text == "bright" ? "/user-nav.svg" : "/user-nav-dark.svg"}
-          height="32"
-          width="32"
-          alt=""
-        />
-      </div>
+      {!data && (
+        <div onClick={handleLogin}>
+          <Image
+            src={text == "bright" ? "/user-nav.svg" : "/user-nav-dark.svg"}
+            height="32"
+            width="32"
+            alt=""
+          />
+        </div>
+      )}
+      {data && (
+        <div className="flex items-center">
+          <p className="text-[0.9375rem] text-[#898989]">
+            {data.user.name.split(" ")[1]}
+          </p>
+          <div className="pr-3" />
+          <Image
+            src={text == "bright" ? "/user-nav.svg" : "/user-nav-dark.svg"}
+            height="32"
+            width="32"
+            alt=""
+          />
+        </div>
+      )}
       {data && (
         <div className="invisible absolute -right-1/2 top-full pt-4 opacity-0 transition-opacity group-hover:visible group-hover:opacity-100">
-          <ul className="relative rounded-lg bg-white py-1 text-black shadow-center-lg after:absolute after:bottom-[100%] after:right-[26%] after:-mb-[5px] after:h-[10px] after:w-[10px] after:translate-x-1/2 after:rotate-45 after:bg-white after:shadow-center-lg">
-            <li className="cursor-pointer px-8 py-1" onClick={handleLogout}>
+          <ul className="relative flex flex-col gap-5 whitespace-nowrap rounded-lg bg-white py-8 font-medium text-black shadow-center-lg">
+            <li className="cursor-pointer px-7">
+              <Link href="/user">Мой профиль</Link>
+            </li>
+            <li className="cursor-pointer px-7">
+              <Link href="/user/teams">Команды</Link>
+            </li>
+            <li className="cursor-pointer px-7">
+              <Link href="/user/projects">Проекты</Link>
+            </li>
+            <li className="cursor-pointer px-7">
+              <Link href="/user/forms">Анкеты</Link>
+            </li>
+            <li
+              className="cursor-pointer px-7 text-primary"
+              onClick={handleLogout}
+            >
               Выйти
             </li>
           </ul>
