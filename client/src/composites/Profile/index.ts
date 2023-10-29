079bf5ea93a37972e0b0bd42c372423504a885f7
Denis Laluev <laluevdenis@yandex.ru>
2023-10-29 17:26:20 +0300

    Add UserFormsPreview unit-tests

--- client/src/composites/Profile/index.ts
+++ client/src/composites/Profile/index.ts
@@ -1,5 +1,9 @@
 import type { Profile } from "./types/types";
 import { useProfileQuery } from "./model/useProfileQuery";
+import {
+  staticProfileTeamAssigned,
+  staticProfileTeamHiring,
+} from "./static/staticProfile";
 
 export type { Profile };
-export { useProfileQuery };
+export { useProfileQuery, staticProfileTeamAssigned, staticProfileTeamHiring };
---
+++ client/src/widgets/UserPreview/ui/__tests__/UserFormsPreview.test.tsx
@@ -0,0 +1,92 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import UserFormsPreview from "../UserFormsPreview";
+import {
+  staticProfileTeamAssigned,
+  useProfileQuery,
+} from "@/composites/Profile";
+import { staticForms } from "@/entities/Form";
+
+jest.mock("@/composites/Profile", () => {
+  const original = jest.requireActual("@/composites/Profile");
+
+  return {
+    ...original,
+    useProfileQuery: jest.fn(),
+  };
+});
+
+describe("UserProfilePreview widget UI", () => {
+  describe("User logged in", () => {
+    beforeAll(() => {
+      (useProfileQuery as jest.Mock).mockReturnValue({
+        data: { ...staticProfileTeamAssigned, forms: staticForms },
+      });
+    });
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("should render the block", () => {
+      const { getByText } = render(<UserFormsPreview />);
+
+      const title = getByText(/Анкеты/i);
+
+      expect(title).toBeInTheDocument();
+    });
+
+    it("should inform the user if no forms are completed", () => {
+      const notCompletedForms = staticForms.map((form) => ({
+        ...form,
+        completed: null,
+      }));
+      (useProfileQuery as jest.Mock).mockReturnValueOnce({
+        data: { ...staticProfileTeamAssigned, forms: notCompletedForms },
+      });
+
+      const { getByText } = render(<UserFormsPreview />);
+
+      const warning = getByText(/У вас нет заполненных анкет/i);
+      expect(warning).toBeVisible();
+    });
+
+    it("should show the number of completed forms", () => {
+      const { getByText } = render(<UserFormsPreview />);
+
+      const count = staticForms.reduce(
+        (sum, cur) => (cur.completed ? sum + 1 : sum),
+        0,
+      );
+
+      const element = getByText(new RegExp("" + count));
+
+      expect(element).toBeInTheDocument();
+    });
+
+    it("should show a redirect button", () => {
+      const { getByText } = render(<UserFormsPreview />);
+
+      const button = getByText(/Заполнить анкету/i);
+
+      expect(button).toBeVisible();
+    });
+  });
+
+  describe("User not logged in", () => {
+    beforeAll(() => {
+      (useProfileQuery as jest.Mock).mockReturnValue({
+        data: null,
+      });
+    });
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+    it("shouldn't render the block", () => {
+      const { queryByText } = render(<UserFormsPreview />);
+
+      const title = queryByText(/Анкеты/i);
+
+      expect(title).not.toBeInTheDocument();
+    });
+  });
+});
