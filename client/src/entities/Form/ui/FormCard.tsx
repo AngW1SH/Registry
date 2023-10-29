e7ae3e76ea1de3808e6d69270f4e371b28af4785
Denis Laluev <laluevdenis@yandex.ru>
2023-10-29 13:43:36 +0300

    Add adaptive UserForms

--- client/src/entities/Form/ui/FormCard.tsx
+++ client/src/entities/Form/ui/FormCard.tsx
@@ -10,38 +10,42 @@ interface FormCardProps {
 
 const FormCard: FC<FormCardProps> = ({ form }) => {
   return (
-    <Block className="flex justify-between rounded-xl px-11 py-12">
-      <p className="w-2/3 font-medium">{form.name}</p>
-      <div className="flex w-1/3 justify-end whitespace-nowrap">
+    <Block className="relative flex flex-col justify-between rounded-xl px-6 py-12 text-center sm:px-11 sm:text-left lg:flex-row">
+      <p className="pb-4 pt-12 text-xl font-medium sm:pl-16 sm:pt-0 lg:w-2/3 lg:pb-0 lg:text-base">
+        {form.name}
+      </p>
+      <div className="flex justify-center whitespace-nowrap sm:justify-start sm:pl-16 lg:w-1/3 lg:justify-end">
         <div>
           {form.completed && (
-            <>
-              <h3 className="text-sm text-[#898989]">Дата прохождения</h3>
-              <p className="text-[0.9375rem]">
-                {form.completed.toLocaleDateString("ru-RU", {
-                  day: "2-digit",
-                  month: "2-digit",
-                  year: "numeric",
-                })}
-              </p>
+            <div className="flex flex-col-reverse gap-x-4 sm:flex-row-reverse lg:flex-col">
+              <div>
+                <h3 className="text-sm text-[#898989]">Дата прохождения</h3>
+                <p className="text-[0.9375rem]">
+                  {form.completed.toLocaleDateString("ru-RU", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
               <div className="pt-3" />
               <Link href={form.link}>
-                <ButtonAlt className="-ml-3 rounded-full border px-6 py-2 text-sm">
+                <ButtonAlt className="rounded-full border px-6 py-2 text-sm lg:-ml-3">
                   Пройти заново
                 </ButtonAlt>
               </Link>
-            </>
+            </div>
           )}
           {!form.completed && (
             <Link href={form.link}>
-              <ButtonAlt className="-ml-3 mt-1 rounded-full border px-12 py-2 text-sm">
+              <ButtonAlt className="mt-1 rounded-full border px-12 py-2 text-sm lg:-ml-3">
                 Пройти
               </ButtonAlt>
             </Link>
           )}
         </div>
-        <div className="pr-14" />
-        <div className="h-10 w-10 min-w-[2.5rem] rounded-full">
+        <div className="hidden pr-14 lg:block" />
+        <div className="absolute left-1/2 top-12 h-10 w-10 min-w-[2.5rem] -translate-x-1/2 rounded-full sm:left-10 sm:top-1/2 sm:-translate-y-1/2 lg:static lg:translate-y-0">
           <div className="relative h-full w-full">
             {form.completed && (
               <Image src="/checked-circle-icon.svg" fill={true} alt="" />
