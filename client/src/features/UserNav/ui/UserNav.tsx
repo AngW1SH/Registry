54be5728a4a6ea343e3f18ef20d59b0b8718b41d
Denis Laluev <laluevdenis@yandex.ru>
2023-11-02 17:21:27 +0300

    Rework UserNav mobile UI

--- client/src/features/UserNav/ui/UserNav.tsx
+++ client/src/features/UserNav/ui/UserNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect, useRef, useState } from "react";
 import { logout } from "../api/logout";
 import { useRouter } from "next/navigation";
 import { useQueryClient } from "@tanstack/react-query";
@@ -34,8 +34,31 @@ const UserNav: FC<UserNavProps> = ({ text = "bright" }) => {
     }
   };
 
+  const [opened, setOpened] = useState(false);
+
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current) {
+        if (
+          e.target instanceof HTMLElement &&
+          !ref.current.contains(e.target)
+        ) {
+          setOpened(false);
+        }
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [ref.current]);
+
   return (
-    <div className="group relative z-[60] cursor-pointer">
+    <div ref={ref} className="group relative z-[60] cursor-pointer">
       {!data && (
         <div onClick={handleLogin}>
           <Image
@@ -47,49 +70,83 @@ const UserNav: FC<UserNavProps> = ({ text = "bright" }) => {
         </div>
       )}
       {data && (
-        <div className="flex flex-col-reverse items-center sm:flex-row">
-          <p className="pt-3 text-xl text-[#898989] sm:pt-0 sm:text-[0.9375rem] ">
-            {data.name.split(" ")[1]}
-          </p>
-          <div className="pr-3" />
+        <div>
           <div
-            className={`relative h-8 w-8 ${
-              text == "bright" ? "sm:block" : ""
-            } hidden`}
+            className="flex items-center sm:hidden"
+            onClick={() => setOpened(true)}
           >
-            <Image src="/user-nav.svg" fill={true} alt="" />
-          </div>
-          <div
-            className={`relative h-14 w-14 sm:h-8 sm:w-8 ${
-              text == "bright" ? "sm:hidden" : ""
-            }`}
-          >
-            <Image src="/user-nav-dark.svg" fill={true} alt="" />
-          </div>
-        </div>
-      )}
-      {data && (
-        <div className="right-0 top-full pr-12 pt-4 transition-opacity sm:invisible sm:absolute sm:pr-0 sm:opacity-0 sm:group-hover:visible sm:group-hover:opacity-100">
-          <ul className="relative flex flex-col gap-7 whitespace-nowrap rounded-lg pt-8 text-xl text-black sm:gap-5 sm:bg-white sm:pb-8 sm:text-base sm:font-medium sm:shadow-center-lg">
-            <li className="cursor-pointer sm:px-7">
-              <Link href="/user">Мой профиль</Link>
-            </li>
-            <li className="cursor-pointer sm:px-7">
-              <Link href="/user/teams">Команды</Link>
-            </li>
-            <li className="cursor-pointer sm:px-7">
-              <Link href="/user/projects">Проекты</Link>
-            </li>
-            <li className="cursor-pointer sm:px-7">
-              <Link href="/user/forms">Анкеты</Link>
-            </li>
-            <li
-              className="cursor-pointer text-primary sm:px-7"
-              onClick={handleLogout}
+            <p
+              className={`${
+                text == "bright" ? "white" : "text-[#898989]"
+              } sm:text-[0.9375rem] `}
             >
-              Выйти
-            </li>
-          </ul>
+              {data.name.split(" ")[1]}
+            </p>
+            <div className="pr-3" />
+            <Image
+              src={text == "bright" ? "/user-nav.svg" : "/user-nav-dark.svg"}
+              height="32"
+              width="32"
+              alt=""
+            />
+          </div>
+          <div
+            className={`fixed right-0 top-0 z-50 h-screen flex-col overflow-auto bg-[#F5F6FA] py-9 pl-6 pr-5 shadow-center-xl transition-transform sm:overflow-visible sm:py-0 sm:text-black ${
+              text == "bright" ? "sm:text-white" : "sm:text-black"
+            } ${
+              opened ? "" : "translate-x-full"
+            } flex sm:static sm:h-auto sm:translate-x-0 sm:flex-row sm:bg-transparent sm:pr-0 sm:shadow-none`}
+          >
+            <div
+              className="relative ml-auto min-h-[32px] min-w-[32px] cursor-pointer sm:hidden"
+              onClick={() => setOpened(false)}
+            >
+              <Image src="/x-gray.svg" alt="Закрыть меню" fill={true} />
+            </div>
+            <div className="pt-10 sm:hidden" />
+            <div className="flex flex-col-reverse items-center sm:flex-row">
+              <p className="pt-3 text-xl text-[#898989] sm:pt-0 sm:text-[0.9375rem] ">
+                {data.name.split(" ")[1]}
+              </p>
+              <div className="pr-3" />
+              <div
+                className={`relative h-8 w-8 ${
+                  text == "bright" ? "sm:block" : ""
+                } hidden`}
+              >
+                <Image src="/user-nav.svg" fill={true} alt="" />
+              </div>
+              <div
+                className={`relative h-14 w-14 sm:h-8 sm:w-8 ${
+                  text == "bright" ? "sm:hidden" : ""
+                }`}
+              >
+                <Image src="/user-nav-dark.svg" fill={true} alt="" />
+              </div>
+            </div>
+            <div className="right-0 top-full pr-12 pt-4 transition-opacity sm:invisible sm:absolute sm:pr-0 sm:opacity-0 sm:group-hover:visible sm:group-hover:opacity-100">
+              <ul className="relative flex flex-col gap-7 whitespace-nowrap rounded-lg pt-8 text-xl text-black sm:gap-5 sm:bg-white sm:pb-8 sm:text-base sm:font-medium sm:shadow-center-lg">
+                <li className="cursor-pointer sm:px-7">
+                  <Link href="/user">Мой профиль</Link>
+                </li>
+                <li className="cursor-pointer sm:px-7">
+                  <Link href="/user/teams">Команды</Link>
+                </li>
+                <li className="cursor-pointer sm:px-7">
+                  <Link href="/user/projects">Проекты</Link>
+                </li>
+                <li className="cursor-pointer sm:px-7">
+                  <Link href="/user/forms">Анкеты</Link>
+                </li>
+                <li
+                  className="cursor-pointer text-primary sm:px-7"
+                  onClick={handleLogout}
+                >
+                  Выйти
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- client/src/widgets/Header/ui/Header.tsx
+++ client/src/widgets/Header/ui/Header.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { UserNav } from "@/features/UserNav";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,31 +8,8 @@ interface HeaderProps {
 }
 
 const Header: FC<HeaderProps> = ({ text = "bright" }) => {
-  const [opened, setOpened] = useState(false);
-
-  const ref = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (e: MouseEvent) => {
-      if (ref.current) {
-        if (
-          e.target instanceof HTMLElement &&
-          !ref.current.contains(e.target)
-        ) {
-          setOpened(false);
-        }
-      }
-    };
-
-    document.addEventListener("click", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, [ref.current]);
-
   return (
-    <div ref={ref}>
+    <div>
       <div className="flex items-center justify-between">
         <div>
           <Link href="/">
@@ -47,11 +23,9 @@ const Header: FC<HeaderProps> = ({ text = "bright" }) => {
           </Link>
         </div>
         <div
-          className={`shadow-center-xl fixed right-0 top-0 z-50 h-screen flex-col overflow-auto bg-[#F5F6FA] py-9 pl-6 pr-5 transition-transform sm:overflow-visible sm:text-black ${
-            text == "bright" ? "sm:text-white" : "sm:text-black"
-          } ${
-            opened ? "" : "translate-x-full"
-          } flex sm:static sm:h-auto sm:translate-x-0 sm:flex-row sm:bg-transparent sm:pr-0 sm:shadow-none`}
+          className={`py-9 pl-6 transition-transform ${
+            text == "bright" ? "text-white" : "text-black"
+          } flex flex-row`}
         >
           <ul className="mr-10 hidden flex-col gap-7 text-xl sm:flex sm:flex-row sm:items-center sm:gap-0 sm:text-base">
             <li className="mr-4">
@@ -59,22 +33,7 @@ const Header: FC<HeaderProps> = ({ text = "bright" }) => {
             </li>
             <li>Заказчикам</li>
           </ul>
-          <div
-            className="relative ml-auto min-h-[32px] min-w-[32px] cursor-pointer sm:hidden"
-            onClick={() => setOpened(false)}
-          >
-            <Image src="/x-gray.svg" alt="Закрыть меню" fill={true} />
-          </div>
-          <div className="pt-10 sm:hidden" />
           <UserNav text={text} />
-        </div>
-        <div className="relative h-10 w-10 cursor-pointer sm:hidden">
-          <Image
-            src="/bars-white-alt.svg"
-            alt=""
-            fill={true}
-            onClick={() => setOpened(true)}
-          />
         </div>
       </div>
     </div>
