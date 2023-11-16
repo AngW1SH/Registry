021e05e47fb14890719d82ce9ff1800439806a6e
Denis Laluev <laluevdenis@yandex.ru>
2023-11-17 00:47:56 +0300

    Change react version

--- strapi/src/plugins/team-builder/admin/src/pages/App/index.tsx
+++ strapi/src/plugins/team-builder/admin/src/pages/App/index.tsx
@@ -5,19 +5,18 @@
  *
  */
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { AnErrorOccurred } from '@strapi/helper-plugin';
-import pluginId from '../../pluginId';
-import HomePage from '../HomePage';
+import React from "react";
+import { Route } from "react-router-dom";
+import { AnErrorOccurred } from "@strapi/helper-plugin";
+import pluginId from "../../pluginId";
+import HomePage from "../HomePage";
 
 const App = () => {
   return (
     <div>
-      <Switch>
-        <Route path={`/plugins/${pluginId}`} component={HomePage} exact />
-        <Route component={AnErrorOccurred} />
-      </Switch>
+      <Route path={`/plugins/${pluginId}`}>
+        <HomePage />
+      </Route>
     </div>
   );
 };
