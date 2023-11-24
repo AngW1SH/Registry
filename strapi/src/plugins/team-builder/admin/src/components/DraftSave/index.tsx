c2f027f9ced210c2c56bc696fb0f33013caf4f31
Denis Laluev <laluevdenis@yandex.ru>
2023-11-24 19:35:01 +0300

    Disable the saveDraft button until there are any changes

--- strapi/src/plugins/team-builder/admin/src/components/DraftSave/index.tsx
+++ strapi/src/plugins/team-builder/admin/src/components/DraftSave/index.tsx
@@ -1,19 +1,39 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 
 import { Button } from "@strapi/design-system";
 import { useDraft } from "../../entities/Draft";
+import { useStudentStore } from "../../entities/Student";
+import { useFormStore } from "../../entities/Form/model";
+import { useDraftTeamsStore } from "../../entities/Team/model";
 
 interface DraftSaveProps {}
 
 const DraftSave: FC<DraftSaveProps> = () => {
   const { draft, saveDraft } = useDraft();
+  const { selectedStudentIds } = useStudentStore();
+  const { selectedForm } = useFormStore();
+  const { teams } = useDraftTeamsStore();
 
-  const onClick = () => {
-    if (draft) saveDraft();
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+
+  const onClick = async () => {
+    if (draft) {
+      await saveDraft();
+      setDisabled(true);
+    }
   };
 
+  useEffect(() => {
+    if (hasLoaded) setDisabled(false);
+  }, [selectedStudentIds, selectedForm, teams, draft]);
+
+  useEffect(() => {
+    if (draft) setHasLoaded(true);
+  }, [draft]);
+
   return (
-    <Button variant="secondary" onClick={onClick}>
+    <Button disabled={disabled} variant="secondary" onClick={onClick}>
       Save Draft
     </Button>
   );
