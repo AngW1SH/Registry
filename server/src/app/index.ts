033188db11e445c632a945fe3d4a9bf360b57ad8
Denis Laluev <laluevdenis@yandex.ru>
2023-12-08 19:01:30 +0300

    Remove express.json() express.urlencoded with the option of extended: true does its job. Using both can lead to unexpected behavior

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import session from "express-session";
@@ -20,9 +20,7 @@ const generateApp = (port?: number) => {
   const app = express();
   if (port) app.listen(port, () => console.log("listening port 8000"));
 
-  app.use(bodyParser.json());
-
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser(process.env.TOKEN_SECRET));
   app.use(fileUpload());
   app.use(
@@ -47,19 +45,6 @@ const generateApp = (port?: number) => {
 
   app.use(errorLogger);
   app.use(errorHandler);
-
-  app.use("/public", express.static(path.resolve(__dirname + "/../public")));
-
-  app.get("/", (req: Request, res: Response) => {
-    res
-      .status(200)
-      .sendFile(path.resolve(__dirname + "/../client/dist/index.html"));
-  });
-
-  app.use(
-    "/assets",
-    express.static(path.resolve(__dirname + "/../client/dist/assets"))
-  );
 
   return app;
 };
