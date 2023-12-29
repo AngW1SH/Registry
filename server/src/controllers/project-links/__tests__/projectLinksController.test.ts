5a480722afb73922a815f44ace79c8f940bf1109
Denis Laluev <laluevdenis@yandex.ru>
2023-12-29 16:24:50 +0300

    Add projectLinksController.addLink tests

---
+++ server/src/controllers/project-links/__tests__/projectLinksController.test.ts
@@ -0,0 +1,110 @@
+import { staticUser } from "@/entities/user";
+import projectLinksController from "..";
+import projectLinksService from "@/services/project-links";
+import {
+  BadRequestError,
+  ServerError,
+  UnauthorizedError,
+} from "@/helpers/errors";
+
+const req: any = {};
+const res: any = {};
+
+jest.mock("@/services/project-links");
+
+req.body = {};
+res.status = jest.fn().mockReturnValue(res);
+res.sendStatus = jest.fn().mockReturnValue(res);
+res.json = jest.fn().mockReturnValue(res);
+
+describe("projectLinksController", () => {
+  describe("addLink method", () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+    it("should call projectLinksService.add when everything is ok", async () => {
+      req.user = staticUser;
+      req.params = { id: 1 };
+      req.body = {
+        platform: "GitHub",
+        link: "TestLink",
+      };
+
+      await projectLinksController.addLink(req, res, jest.fn());
+
+      expect(projectLinksService.add).toHaveBeenCalledWith(
+        +req.params.id,
+        req.body.platform,
+        req.body.link,
+        req.user
+      );
+    });
+    it("should send a 200 status when everything is ok", async () => {
+      req.user = staticUser;
+      req.params = { id: 1 };
+      req.body = {
+        platform: "GitHub",
+        link: "TestLink",
+      };
+
+      await projectLinksController.addLink(req, res, jest.fn());
+
+      expect(res.status).toHaveBeenCalledWith(200);
+    });
+    it("should pass errors to middleware", async () => {
+      (projectLinksService.add as jest.Mock).mockImplementationOnce(
+        async () => {
+          throw new ServerError("");
+        }
+      );
+      req.user = staticUser;
+      req.params = { id: 1 };
+      req.body = {
+        platform: "GitHub",
+        link: "TestLink",
+      };
+      const nextMock = jest.fn();
+
+      await projectLinksController.addLink(req, res, nextMock);
+
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(ServerError);
+    });
+    it("should throw UnauthorizedError when no user is specified in req.user", async () => {
+      req.user = null;
+      req.params = { id: 1 };
+      req.body = {
+        platform: "GitHub",
+        link: "TestLink",
+      };
+      const nextMock = jest.fn();
+
+      await projectLinksController.addLink(req, res, nextMock);
+
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(UnauthorizedError);
+    });
+    it("should throw BadRequestError when the 'platform' parameter is not specified", async () => {
+      req.user = staticUser;
+      req.params = { id: 1 };
+      req.body = {
+        link: "TestLink",
+      };
+      const nextMock = jest.fn();
+
+      await projectLinksController.addLink(req, res, nextMock);
+
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(BadRequestError);
+    });
+    it("should throw BadRequestError when the 'link' parameter is not specified", async () => {
+      req.user = staticUser;
+      req.params = { id: 1 };
+      req.body = {
+        platform: "GitHub",
+      };
+      const nextMock = jest.fn();
+
+      await projectLinksController.addLink(req, res, nextMock);
+
+      expect(nextMock.mock.calls[0][0]).toBeInstanceOf(BadRequestError);
+    });
+  });
+});
