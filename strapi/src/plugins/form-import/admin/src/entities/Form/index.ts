127c3c1fc4a66ae6e44e082aec1c70f222c0ce78
Denis Laluev <laluevdenis@yandex.ru>
2024-01-05 17:14:38 +0300

    Add Form types

---
+++ strapi/src/plugins/form-import/admin/src/entities/Form/index.ts
@@ -0,0 +1,8 @@
+import type {
+  IForm,
+  IFormQuestion,
+  IFormQuestionDefault,
+  IFormQuestionGrid,
+} from "./types";
+
+export type { IForm, IFormQuestion, IFormQuestionDefault, IFormQuestionGrid };
---
+++ strapi/src/plugins/form-import/admin/src/entities/Form/types/index.ts
@@ -0,0 +1,18 @@
+export interface IForm {
+  type: string | null;
+  results: IFormQuestion[][];
+  selected: number[]; // the indices of the selected results
+}
+
+export type IFormQuestion = IFormQuestionDefault | IFormQuestionGrid;
+
+export interface IFormQuestionDefault {
+  type: "DEFAULT";
+  question: string;
+}
+
+export interface IFormQuestionGrid {
+  type: "GRID";
+  question: string;
+  rows: string[];
+}
