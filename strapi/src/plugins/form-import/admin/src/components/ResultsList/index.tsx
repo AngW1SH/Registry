1b6cb7bbd52d48926b45514ea13f6100176fc00f
Denis Laluev <laluevdenis@yandex.ru>
2024-01-06 15:00:02 +0300

    Display actual form results

--- strapi/src/plugins/form-import/admin/src/components/ResultsList/index.tsx
+++ strapi/src/plugins/form-import/admin/src/components/ResultsList/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import {
   Table,
   Thead,
@@ -15,6 +15,7 @@ import {
 } from "@strapi/design-system";
 import { Pencil } from "@strapi/icons";
 import { ClickableTr } from "./styles";
+import { IFormQuestionDefault, useFormStore } from "../../entities/Form";
 
 interface ResultsListProps {}
 
@@ -22,20 +23,34 @@ const ResultsList: FC<ResultsListProps> = () => {
   const ROW_COUNT = 6;
   const COL_COUNT = 10;
 
-  const results = [
-    {
-      name: "Иванов Сергей Александрович",
-      date: new Date(),
-    },
-    {
-      name: "Иванов Сергей Алекса2ндрович",
-      date: new Date(),
-    },
-    {
-      name: "Иванов Сергей Алекс1андрович",
-      date: new Date(),
-    },
-  ];
+  const { results, selected } = useFormStore();
+
+  const formattedResults = results
+    .filter((result, index) => selected.includes(index))
+    .map((result) => {
+      const nameFind = result.find(
+        (row) => row.question == "Фамилия Имя Отчество"
+      );
+      const emailFind = result.find(
+        (row) => row.question == "Электронная почта, указанная при авторизации"
+      );
+      const dateFind = result.find((row) => (row.question = "Timestamp"));
+
+      return {
+        name: nameFind && nameFind.type == "DEFAULT" ? nameFind.answer : "",
+        email: emailFind && emailFind.type == "DEFAULT" ? emailFind.answer : "",
+        date:
+          dateFind && dateFind.type == "DEFAULT"
+            ? new Date(dateFind.answer).toLocaleTimeString("ru-RU", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+              })
+            : "",
+      };
+    });
 
   return (
     <Table colCount={COL_COUNT} rowCount={ROW_COUNT} onClick={() => {}}>
@@ -51,13 +66,16 @@ const ResultsList: FC<ResultsListProps> = () => {
             <Typography variant="sigma">Name</Typography>
           </Th>
           <Th>
+            <Typography variant="sigma">Email</Typography>
+          </Th>
+          <Th>
             <Typography variant="sigma">Date</Typography>
           </Th>
         </Tr>
       </Thead>
       <Tbody>
-        {results.map((result, index) => (
-          <ClickableTr key={result.date.toDateString() + result.name}>
+        {formattedResults.map((result, index) => (
+          <ClickableTr key={result.email + result.date}>
             <Td>
               <BaseCheckbox aria-label={`Select ${index + 1}`} />
             </Td>
@@ -68,13 +86,10 @@ const ResultsList: FC<ResultsListProps> = () => {
               <Typography textColor="neutral800">{result.name}</Typography>
             </Td>
             <Td>
-              <Typography textColor="neutral800">
-                {new Date(result.date).toLocaleDateString("ru-Ru", {
-                  day: "2-digit",
-                  month: "2-digit",
-                  year: "numeric",
-                })}
-              </Typography>
+              <Typography textColor="neutral800">{result.email}</Typography>
+            </Td>
+            <Td>
+              <Typography textColor="neutral800">{result.date}</Typography>
             </Td>
           </ClickableTr>
         ))}
