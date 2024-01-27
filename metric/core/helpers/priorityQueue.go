9574b8037a3ccc75a1c1c3a12c5f968885eda09d
Denis Laluev <laluevdenis@yandex.ru>
2024-01-27 17:38:05 +0300

    feat: add priority queue

---
+++ metric/core/helpers/priorityQueue.go
@@ -0,0 +1,29 @@
+package helpers
+
+import "core/models"
+
+type PriorityQueue []models.Task
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+    return pq[i].UpdatedAt.Before(pq[j].UpdatedAt)
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+    pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+    item := *x.(*models.Task)
+    *pq = append(*pq, item)
+}
+
+// Pop removes the smallest element (by updatedAt) from the priority queue.
+func (pq *PriorityQueue) Pop() interface{} {
+    old := *pq
+    n := len(old)
+    item := old[n-1]
+    *pq = old[0 : n-1]
+    return item
+}
