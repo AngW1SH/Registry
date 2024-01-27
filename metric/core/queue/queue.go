3f496a367ad2f60592348b84d4a0bec674f1b8a4
Denis Laluev <laluevdenis@yandex.ru>
2024-01-27 17:46:15 +0300

    feat: initialize priority queue in InitializeQueue

--- metric/core/queue/queue.go
+++ metric/core/queue/queue.go
@@ -1,6 +1,8 @@
 package queue
 
 import (
+	"container/heap"
+	"core/helpers"
 	"core/metrics"
 	"core/models"
 	"time"
@@ -8,18 +10,25 @@ import (
 
 var limit int
 
-var queue []models.Task
+var queue helpers.PriorityQueue
 
 func InitializeQueue(lim int) {
 	limit = lim
 
-	queue = []models.Task{ 
+	tasks := []models.Task{ 
 		{ Metric: "1", Data: []string{ "1-1", "1-2" }, UpdatedAt: time.Now(), Weight: 1 },
 		{ Metric: "2", Data: []string{ "2" }, UpdatedAt: time.Now(), Weight: 2 },
 		{ Metric: "1", Data: []string{ "1-3", "1-4" }, UpdatedAt: time.Now(), Weight: 3 },
 		{ Metric: "3", Data: []string{ "3" }, UpdatedAt: time.Now(), Weight: 4 },
 		{ Metric: "2", Data: []string{ "2" }, UpdatedAt: time.Now(), Weight: 1 },
 	}
+
+	queue = make(helpers.PriorityQueue, len(tasks))
+
+	for i, obj := range(tasks) {
+		queue[i] = &obj
+	}
+	heap.Init(&queue)
 
 	AdvanceTasks()
 }
