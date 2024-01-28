ee062d6c9f4f76e263cfc46038e5c102b97fbcfd
Denis Laluev <laluevdenis@yandex.ru>
2024-01-28 18:40:41 +0300

    refactor: make AdvanceTasks an infinite loop and move task defining to main() Calling AdvanceTasks in each callback could create multiple instances running at the same time and lead to unexpected behaviour

--- metric/core/queue/queue.go
+++ metric/core/queue/queue.go
@@ -12,18 +12,11 @@ var limit, load int
 
 var queue helpers.PriorityQueue
 
-
 func InitializeQueue(lim int) {
 	limit = lim
 	load = 0
 
-	tasks := []models.Task{ 
-		{ Metric: "1", Data: []string{ "1-1", "1-2" }, UpdatedAt: time.Date(2024, time.January, 28, 12, 0, 0, 0, time.UTC), Weight: 1, UpdateRate: 20 * time.Second },
-		{ Metric: "2", Data: []string{ "2" }, UpdatedAt: time.Date(2024, time.January, 28, 13, 0, 0, 0, time.UTC), UpdateRate: 20 * time.Second },
-		{ Metric: "1", Data: []string{ "1-3", "1-4" }, UpdatedAt: time.Date(2024, time.January, 28, 14, 0, 0, 0, time.UTC), UpdateRate: 20 * time.Second },
-		{ Metric: "3", Data: []string{ "3" }, UpdatedAt: time.Date(2024, time.January, 28, 10, 0, 0, 0, time.UTC), Weight: 4, UpdateRate: 20 * time.Second },
-		{ Metric: "2", Data: []string{ "2" }, UpdatedAt: time.Date(2024, time.January, 28, 9, 0, 0, 0, time.UTC), Weight: 1, UpdateRate: 20 * time.Second },
-	}
+	tasks := []models.Task{ }
 
 	queue = make(helpers.PriorityQueue, len(tasks))
 
@@ -34,7 +27,7 @@ func InitializeQueue(lim int) {
 	}
 	heap.Init(&queue)
 
-	AdvanceTasks()
+	go AdvanceTasks()
 }
 
 func AddTask(task models.Task) {
@@ -49,34 +42,39 @@ func onFinish(task models.Task) {
 	task.AttemptedAt = time.Now()
 
 	heap.Push(&queue, &task)
-
-	AdvanceTasks()
 }
 
 func AdvanceTasks() {
-
-	for load + queue.Peek().Weight <= limit {
+	for ;; {
 
 		if (queue.Len() == 0) {
 			time.Sleep(time.Second)
-			continue
+			continue;
 		}
-		
-		oldestUpdated := heap.Pop(&queue).(*models.Task)
-		found := false
 
-		if metrics.List[oldestUpdated.Metric] != nil {
-			if time.Since(oldestUpdated.UpdatedAt) > oldestUpdated.UpdateRate {
-				metrics.Run(*oldestUpdated, metrics.List[oldestUpdated.Metric], onFinish);
-				found = true
-			} else {
-				oldestUpdated.AttemptedAt = time.Now()
-				heap.Push(&queue, oldestUpdated)
+		for load < limit {
+			if queue.Len() == 0 || (load + queue.Peek().Weight > limit) {
+				break
+			}
+	
+			oldestUpdated := heap.Pop(&queue).(*models.Task)
+			found := false
+	
+			if metrics.List[oldestUpdated.Metric] != nil {
+				if time.Since(oldestUpdated.UpdatedAt) > oldestUpdated.UpdateRate {
+					metrics.Run(*oldestUpdated, metrics.List[oldestUpdated.Metric], onFinish);
+					found = true
+				} else {
+					oldestUpdated.AttemptedAt = time.Now()
+					heap.Push(&queue, oldestUpdated)
+				}
+			}
+	
+			if found {
+				load += oldestUpdated.Weight		
 			}
 		}
 
-		if found {
-			load += oldestUpdated.Weight		
-		}
+		time.Sleep(time.Second)
 	}
 }
