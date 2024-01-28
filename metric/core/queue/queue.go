ea5effc7f0c119bb0916c6375e419f020c482f6d
Denis Laluev <laluevdenis@yandex.ru>
2024-01-28 18:14:20 +0300

    refactor: initialize task.AttemptedAt when creating queue, instead of passing it as a parameter AttemptedAt is purely technical and shouldn't be a part of the business logic

--- metric/core/queue/queue.go
+++ metric/core/queue/queue.go
@@ -18,17 +18,18 @@ func InitializeQueue(lim int) {
 	load = 0
 
 	tasks := []models.Task{ 
-		{ Metric: "1", Data: []string{ "1-1", "1-2" }, UpdatedAt: time.Date(2024, time.January, 28, 12, 0, 0, 0, time.UTC), AttemptedAt: time.Date(2024, time.January, 28, 12, 0, 0, 0, time.UTC), Weight: 1, UpdateRate: 20 * time.Second },
-		{ Metric: "2", Data: []string{ "2" }, UpdatedAt: time.Date(2024, time.January, 28, 13, 0, 0, 0, time.UTC), Weight: 2, AttemptedAt: time.Date(2024, time.January, 28, 13, 0, 0, 0, time.UTC), UpdateRate: 20 * time.Second },
-		{ Metric: "1", Data: []string{ "1-3", "1-4" }, UpdatedAt: time.Date(2024, time.January, 28, 14, 0, 0, 0, time.UTC), AttemptedAt: time.Date(2024, time.January, 28, 14, 0, 0, 0, time.UTC), UpdateRate: 20 * time.Second },
-		{ Metric: "3", Data: []string{ "3" }, UpdatedAt: time.Date(2024, time.January, 28, 10, 0, 0, 0, time.UTC), Weight: 4, AttemptedAt: time.Date(2024, time.January, 28, 10, 0, 0, 0, time.UTC), UpdateRate: 20 * time.Second },
-		{ Metric: "2", Data: []string{ "2" }, UpdatedAt: time.Date(2024, time.January, 28, 9, 0, 0, 0, time.UTC), Weight: 1, AttemptedAt: time.Date(2024, time.January, 28, 9, 0, 0, 0, time.UTC), UpdateRate: 20 * time.Second },
+		{ Metric: "1", Data: []string{ "1-1", "1-2" }, UpdatedAt: time.Date(2024, time.January, 28, 12, 0, 0, 0, time.UTC), Weight: 1, UpdateRate: 20 * time.Second },
+		{ Metric: "2", Data: []string{ "2" }, UpdatedAt: time.Date(2024, time.January, 28, 13, 0, 0, 0, time.UTC), UpdateRate: 20 * time.Second },
+		{ Metric: "1", Data: []string{ "1-3", "1-4" }, UpdatedAt: time.Date(2024, time.January, 28, 14, 0, 0, 0, time.UTC), UpdateRate: 20 * time.Second },
+		{ Metric: "3", Data: []string{ "3" }, UpdatedAt: time.Date(2024, time.January, 28, 10, 0, 0, 0, time.UTC), Weight: 4, UpdateRate: 20 * time.Second },
+		{ Metric: "2", Data: []string{ "2" }, UpdatedAt: time.Date(2024, time.January, 28, 9, 0, 0, 0, time.UTC), Weight: 1, UpdateRate: 20 * time.Second },
 	}
 
 	queue = make(helpers.PriorityQueue, len(tasks))
 
 	for i := 0; i < len(tasks); i++ {
 		task := &tasks[i]
+		task.AttemptedAt = task.UpdatedAt
 		queue[i] = task
 	}
 	heap.Init(&queue)
