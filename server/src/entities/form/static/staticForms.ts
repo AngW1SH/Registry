2aadd4e65a37697d3496bac99ec5fe969317a3f5
Denis Laluev <laluevdenis@yandex.ru>
2024-01-31 17:40:28 +0300

    test: add 'type' to staticForms

--- server/src/entities/form/static/staticForms.ts
+++ server/src/entities/form/static/staticForms.ts
@@ -1,14 +1,16 @@
-import { Form } from "..";
+import { Form, FormType } from "..";
 
 export const staticForms: Form[] = [
   {
     id: 1,
     name: "Анкета для студентов ПМ-ПУ",
     link: "https://test.com/form",
+    type: FormType.google,
   },
   {
     id: 2,
     name: "Анкета для политологов",
     link: "https://test.com/otherform",
+    type: FormType.google,
   },
 ];
