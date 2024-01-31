70021e25ecaab826a5e244f2f4e63d30e08f9752
Denis Laluev <laluevdenis@yandex.ru>
2024-01-31 18:41:25 +0300

    test: add projectlinksService.deleteLink tests and a single projectLinksService.add test

--- server/src/services/project-links/__tests__/projectLinksService.test.ts
+++ server/src/services/project-links/__tests__/projectLinksService.test.ts
@@ -9,6 +9,7 @@ import {
 import { staticProjectList } from "@/entities/project";
 import platformRepository from "@/repositories/platform";
 import { staticPlatformList } from "@/entities/platform";
+import projectLinksRepository from "@/repositories/project-links";
 
 jest.mock("@/repositories/project");
 jest.mock("@/repositories/platform");
@@ -16,6 +17,9 @@ jest.mock("@/repositories/project-links");
 
 describe("projectLinksServices", () => {
   describe("add method", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
     it("should fetch the project", async () => {
       try {
         await projectLinksService.add(
@@ -116,6 +120,100 @@ describe("projectLinksServices", () => {
       expect(
         projectLinksService.add("1", "GitHub", "https://example.com", user)
       ).rejects.toThrow(ServerError);
+    });
+
+    it("should call the repository's add method if everything is ok", async () => {
+      const user = {
+        id: 1,
+        name: "User",
+        email: "user@test.com",
+      };
+
+      (projectRepository.findOne as jest.Mock).mockReturnValueOnce({
+        project: staticProjectList[0],
+        administrators: [user],
+      });
+      (platformRepository.findOne as jest.Mock).mockReturnValueOnce(
+        staticPlatformList[0]
+      );
+
+      await projectLinksService.add("1", "GitHub", "https://example.com", user);
+
+      expect(projectLinksRepository.add).toHaveBeenCalled();
+    });
+  });
+
+  describe("deleteLink method", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+    it("should fetch the project", async () => {
+      try {
+        await projectLinksService.deleteLink("1", 1, staticUser);
+      } catch {}
+
+      expect(projectRepository.findOne).toHaveBeenCalled();
+    });
+
+    it("should throw an error if the project is not found", async () => {
+      (projectRepository.findOne as jest.Mock).mockReturnValueOnce(null);
+
+      expect(
+        projectLinksService.deleteLink("1", 1, staticUser)
+      ).rejects.toThrow(ServerError);
+    });
+
+    it("should throw an error if the project's administrators property is not defined", async () => {
+      (projectRepository.findOne as jest.Mock).mockReturnValueOnce({
+        project: staticProjectList[0],
+      });
+
+      expect(
+        projectLinksService.deleteLink("1", 1, staticUser)
+      ).rejects.toThrow(ServerError);
+    });
+
+    it("should throw an error if the user is not an administrator", async () => {
+      (projectRepository.findOne as jest.Mock).mockReturnValueOnce({
+        project: staticProjectList[0],
+        administrators: [
+          {
+            id: 1,
+            name: "Test",
+            email: "test@test.com",
+          },
+        ],
+      });
+
+      const user = {
+        id: 2,
+        name: "User",
+        email: "user@test.com",
+      };
+
+      expect(projectLinksService.deleteLink("1", 1, user)).rejects.toThrow(
+        UnauthorizedError
+      );
+    });
+
+    it("should call the repository's deleteLink method if everything is ok", async () => {
+      const user = {
+        id: 1,
+        name: "User",
+        email: "user@test.com",
+      };
+
+      (projectRepository.findOne as jest.Mock).mockReturnValueOnce({
+        project: staticProjectList[0],
+        administrators: [user],
+      });
+      (platformRepository.findOne as jest.Mock).mockReturnValueOnce(
+        staticPlatformList[0]
+      );
+
+      await projectLinksService.deleteLink("1", 1, user);
+
+      expect(projectLinksRepository.deleteLink).toHaveBeenCalled();
     });
   });
 });
