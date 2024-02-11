305fbcc949e1fc350e49767dc82763a88a96cbe7
Denis Laluev <laluevdenis@yandex.ru>
2024-02-11 12:36:23 +0300

    refactor: create 'src/pages' directory for Layout contents

--- metric/admin/src/app/App.tsx
+++ metric/admin/src/app/App.tsx
@@ -1,8 +1,13 @@
+import { ProjectPage } from "@/pages/Project";
 import { Layout } from "@/widgets/Layout";
 import { Sidebar } from "@/widgets/Sidebar";
 
 function App() {
-  return <Layout aside={<Sidebar />}>123</Layout>;
+  return (
+    <Layout aside={<Sidebar />}>
+      <ProjectPage />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ metric/admin/src/pages/Project/ui/ProjectPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface ProjectPageProps {}
+
+const ProjectPage: FC<ProjectPageProps> = () => {
+  return <div>1</div>;
+};
+
+export default ProjectPage;
