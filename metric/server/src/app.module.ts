ba132e21d23cdeba6edd91e49a7119708036441c
Denis Laluev <laluevdenis@yandex.ru>
2024-03-04 17:21:06 +0300

    feat: add PrismaModule

--- metric/server/src/app.module.ts
+++ metric/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { PlatformModule } from './platform/platform.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TaskModule, PlatformModule],
+  imports: [TaskModule, PlatformModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
