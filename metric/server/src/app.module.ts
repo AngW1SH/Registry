56101779af1fc6b959c354aedf2aa81c5dd01e2c
Denis Laluev <laluevdenis@yandex.ru>
2024-03-04 17:06:43 +0300

    feat: generate paltform module

--- metric/server/src/app.module.ts
+++ metric/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
+import { PlatformModule } from './platform/platform.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule, PlatformModule],
   controllers: [AppController],
   providers: [AppService],
 })
