4bfff1679e4fa805dc7022b9798c71399b45704b
Denis Laluev <laluevdenis@yandex.ru>
2024-03-28 15:56:07 +0300

    feat: update TotalCommits logic to better match its name

--- metric/admin/src/entities/Metric/instances/TotalCommits/model/calculate.ts
+++ metric/admin/src/entities/Metric/instances/TotalCommits/model/calculate.ts
@@ -1,5 +1,15 @@
-import { CommitsPerDay } from "../types/validate";
+import { TotalCommits } from "../types/validate";
 
-export const calculate = (data: CommitsPerDay) => {
-  return data.length;
+export const calculate = (data: TotalCommits) => {
+  let maxDate = new Date("01.01.1970");
+  let result = -1;
+
+  data.forEach((x) => {
+    if (x.timestamp > maxDate) {
+      maxDate = x.timestamp;
+      result = x.data.value;
+    }
+  });
+
+  return result === -1 ? 0 : result;
 };
--- metric/admin/src/entities/Metric/instances/TotalCommits/utils/generateGraphData.ts
+++ metric/admin/src/entities/Metric/instances/TotalCommits/utils/generateGraphData.ts
@@ -4,18 +4,30 @@ import { TotalCommits } from "../types/validate";
 export const generateGraphData = (
   data: TotalCommits
 ): { values: number[]; labels: string[] } => {
-  const values: number[] = monthNames.map(() => 0);
+  const values: {
+    date: Date;
+    value: number;
+  }[] = monthNames.map(() => {
+    return { date: new Date("01.01.1970"), value: -1 };
+  });
 
   data.forEach((x) => {
-    values[x.timestamp.getMonth()] += 1;
+    const month = x.timestamp.getMonth();
+
+    if (values[month].date < x.timestamp) {
+      values[month] = {
+        date: x.timestamp,
+        value: x.data.value,
+      };
+    }
   });
 
   const labels = monthNames.filter((_, i) => {
-    return values[i] !== 0;
+    return values[i].value !== -1;
   });
 
   return {
-    values: values.filter((val) => val),
+    values: values.filter((val) => val.value != -1).map((val) => val.value),
     labels,
   };
 };
