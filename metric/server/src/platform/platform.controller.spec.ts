f0e664f2e718482c7480ce51067e5f20098846a9
Denis Laluev <laluevdenis@yandex.ru>
2024-04-02 17:20:43 +0300

    test: edit jest config and add platform controller tests

---
+++ metric/server/src/platform/platform.controller.spec.ts
@@ -0,0 +1,70 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PlatformController } from './platform.controller';
+import { PlatformService } from './platform.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { platformMocks } from './platform.mock';
+
+describe('PlatformController', () => {
+  let controller: PlatformController;
+  let service: PlatformService;
+  let prisma: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PlatformController],
+      providers: [PlatformService, PrismaService],
+    }).compile();
+
+    controller = module.get<PlatformController>(PlatformController);
+    service = module.get<PlatformService>(PlatformService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('findAll method', () => {
+    it('should call the platformService.findAll method', async () => {
+      const result = platformMocks;
+
+      jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+
+      await controller.findAll();
+
+      expect(service.findAll).toHaveBeenCalled();
+    });
+    it('should return the found platforms', async () => {
+      const result = platformMocks;
+
+      jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+
+      expect(await controller.findAll()).toEqual(result);
+    });
+  });
+
+  describe('findOne method', () => {
+    it('should call the platformService.findOne method', async () => {
+      const result = platformMocks[0];
+
+      jest.spyOn(service, 'findOne').mockImplementation(async () => result);
+
+      await controller.findOne(result.id);
+
+      expect(service.findOne).toHaveBeenCalled();
+      expect(service.findOne).toHaveBeenCalledWith(result.id);
+    });
+    it('should return the found platform', async () => {
+      const result = platformMocks[0];
+
+      jest.spyOn(service, 'findOne').mockImplementation(async () => result);
+
+      expect(await controller.findOne(result.id)).toEqual(result);
+    });
+    it("should return null if the platform doesn't exist", async () => {
+      jest.spyOn(service, 'findOne').mockImplementation(async () => null);
+
+      expect(await controller.findOne('0')).toBeNull();
+    });
+  });
+});
--- metric/server/src/platform/platform.service.ts
+++ metric/server/src/platform/platform.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { Platform } from './platform.entity';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PlatformService {
