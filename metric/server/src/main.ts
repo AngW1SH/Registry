08cf1ea475fc99fa3b7c20ecd26d3190adb65f46
Denis Laluev <laluevdenis@yandex.ru>
2024-05-10 13:38:46 +0300

    chore: add swagger dependency

--- metric/server/src/main.ts
+++ metric/server/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Metric Server')
+    .setDescription('The metric server API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4173);
 }
 bootstrap();
